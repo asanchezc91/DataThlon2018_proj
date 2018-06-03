2401e88f8747b68465e948c93423cba48b99e524
AlvaroSanchez91 <alvaro.sanchez91@hotmail.com>
2018-06-03 13:39:32 +0000

    explore...

--- alvaro_explore.R
+++ alvaro_explore.R
@@ -13,9 +13,17 @@ main.path <- '~'
 data.path <- 'datasets/'
 
 biomass <- data.table(read_xlsx(paste0(data.path, 'biomass_production_adm_2.xlsx')))
-datos <- data.table(read_xlsx(paste0(data.path, 'datos_dathaton.xlsx')))
+datos <- data.table(read_xlsx(paste0(data.path, 'datos_dathaton.xlsx'), col_types = c(rep('guess',4), 'date', rep('guess',30))))
 health <- data.table(read_xlsx(paste0(data.path, 'HEALTH_DISTRICT_DATA.xlsx')))
-inform <- data.table(read_xlsx(paste0(data.path, 'INFORM_Mid2018_v034.xlsx')))
+
+#inform tiene sólo algunas hojas que nos interesan:
+
+sheet.names <- excel_sheets(paste0(data.path, 'INFORM_Mid2018_v034.xlsx'))
+sheet.names <- gsub(' ', '_', sheet.names)
+
+for (i in 3:length(sheet.names)){
+  assign(paste0('inform_', sheet.names[i]), data.table(read_xlsx(paste0(data.path, 'INFORM_Mid2018_v034.xlsx'), sheet = i)))
+}
 niger <- data.table(read_xlsx(paste0(data.path, 'NIGER_BD_Surveillance_Pastorale_2015-2017.xlsx')))
 reasons <- data.table(read_xlsx(paste0(data.path, 'REASONS_FOR_NON_ATTENDANCE.xlsx')))
 weighted <- data.table(read_xlsx(paste0(data.path, 'WEIGHTED_BARRIERS.xlsx')))
@@ -25,4 +33,5 @@ weighted <- data.table(read_xlsx(paste0(data.path, 'WEIGHTED_BARRIERS.xlsx')))
 #coords
 
 health
+
 
