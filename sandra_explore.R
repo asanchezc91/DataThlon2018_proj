df324ea70f39d589bb9736c7a819a50d3b94b38d
sandra3492 <sandra.munarm@gmail.com>
2018-06-03 15:25:11 +0000

    Scripts

--- sandra_explore.R
+++ sandra_explore.R
@@ -7,7 +7,7 @@ main.path <- '~'
 data.path <- 'datasets/'
 
 biomass <- data.table(read_xlsx(paste0(data.path, 'biomass_production_adm_2.xlsx')))
-datos <- data.table(read_xlsx(paste0(data.path, 'datos_dathaton.xlsx')))
+datos <- data.table(read_xlsx(paste0(data.path, 'datos_dathaton.xlsx'), col_types = c(rep('guess',4), 'date', rep('guess',30))))
 health <- data.table(read_xlsx(paste0(data.path, 'HEALTH_DISTRICT_DATA.xlsx')))
 
 #inform tiene sólo algunas hojas que nos interesan:
@@ -18,14 +18,32 @@ sheet.names <- gsub(' ', '_', sheet.names)
 for (i in 3:length(sheet.names)){
   assign(paste0('inform_', sheet.names[i]), data.table(read_xlsx(paste0(data.path, 'INFORM_Mid2018_v034.xlsx'), sheet = i)))
 }
+
 niger <- data.table(read_xlsx(paste0(data.path, 'NIGER_BD_Surveillance_Pastorale_2015-2017.xlsx')))
 reasons <- data.table(read_xlsx(paste0(data.path, 'REASONS_FOR_NON_ATTENDANCE.xlsx')))
 weighted <- data.table(read_xlsx(paste0(data.path, 'WEIGHTED_BARRIERS.xlsx')))
 
+datasets <- c('biomass', 'datos', 'health', 'niger', 'reasons', 'weighted')
+
+for (n in datasets){
+  setnames(get(n), names(get(n)), gsub(" ","_",names(get(n))))
+}
 
 
-#coords
+dtb <-`inform_INFORM_Mid2018_(a-z)`
+dtb <- dtb[2:nrow(dtb),2:8, with=FALSE]
+dtb <- dtb[ISO3 %in% unique(datos$iso_country)]
+dtb <- melt(dtb, id = 'ISO3', measure.vars = setdiff(names(dtb), 'ISO3'))
 
-health
+health[, HD_Code:= substr(HD_Code, 1, 3)]
 
-# distrito, pais, ID, lat, long, localización, tipo de población, link y size
+merge(dtb, health, by.x='ISO3', by.y = 'HD_Code', all.x=TRUE)
+
+#Data Merging
+
+# Health y datos
+
+prueba<- merge(biomass, health, by.x = 'ADM1_NAME', by.y = 'Health_District', all.x=TRUE)
+
+
+#Cluster_Medoids
